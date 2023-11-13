3568afb14f719fefb4a50e989f19bb139d7f32af
ilias1313 <iliasdu1313@gmail.com>
2023-11-13 08:24:29 +0100

    rajout du Scout.cpp

---
+++ src/shared/state/Scout.cpp
@@ -0,0 +1,34 @@
+#include "Scout.h"
+
+namespace state {
+
+    // Constructeur de Scout
+    Scout::Scout() {
+
+
+    }
+
+    // Destructeur de Scout
+    Scout::~Scout() {
+        // Nettoyage spécifique à Scout, si nécessaire
+    }
+
+    // Méthodes spécifiques à Scout
+    void Scout::createVote() {
+        // Implémentation de la méthode createVote
+    }
+
+    void Scout::createFinalVote() {
+        // Implémentation de la méthode createFinalVote
+    }
+
+    void Scout::distributeEquipments() {
+        // Implémentation de la méthode distributeEquipments
+    }
+
+    void Scout::distributeFinalClues() {
+        // Implémentation de la méthode distributeFinalClues
+    }
+
+
+}
