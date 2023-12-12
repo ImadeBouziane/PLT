c3196039565e6bf715614cb1d48c8936a422d4e5
ilias1313 <iliasdu1313@gmail.com>
2023-12-12 17:01:40 +0100

    test

---
+++ src/shared/engine/Command.cpp
@@ -0,0 +1,32 @@
+// Command.cpp
+#include "Command.h"
+
+
+namespace engine {
+
+    // Constructor
+    Command::Command() = default;
+
+    // Destructor
+    Command::~Command() = default;
+
+    // Getter for CommandTypeId
+    CommandTypeId Command::getCommandTypeId() {
+        return commandTypeId;
+    }
+
+    // Execute method
+    void Command::execute(state::GameState &state) {
+    }
+
+    // Serialize method
+    void Command::serialize() {
+
+    }
+
+    bool Command::check(state::Game &state) {
+        return authorPlayer==state.playing;
+    }
+
+
+} // namespace engine
