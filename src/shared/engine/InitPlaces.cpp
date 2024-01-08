20ba4b56c192f944ddb82fc7bc5053d4e004781c
TristanTestu <tristan.testu@gmail.com>
2024-01-08 14:50:23 +0100

    Finition InitPlaces

--- src/shared/engine/InitPlaces.cpp
+++ src/shared/engine/InitPlaces.cpp
@@ -12,9 +12,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place1.setIdPlace("Crique Secrète");
         place1.setIsSafe(false);
         place1.setIsTrapped(true);
-        //place1.setListClues();
-        //place1.setPresentPlayers();
-        //place1.setTrap();
+        place1.setListClues({});
+        place1.setPresentPlayers({});
+        place1.setTrap("");
 
         placesDeck.push_back(place1); 
 
@@ -22,9 +22,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place2.setIdPlace("Salon Tropical");
         place2.setIsSafe(false);
         place2.setIsTrapped(true);
-        //place2.setListClues();
-        //place2.setPresentPlayers();
-        //place2.setTrap();
+        place2.setListClues({});
+        place2.setPresentPlayers({});
+        place2.setTrap("");
 
         placesDeck.push_back(place2); 
 
@@ -32,9 +32,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place3.setIdPlace("Poste de Secours");
         place3.setIsSafe(false);
         place3.setIsTrapped(true);
-        //place3.setListClues();
-        //place3.setPresentPlayers();
-        //place3.setTrap();
+        place3.setListClues({});
+        place3.setPresentPlayers({});
+        place3.setTrap("");
 
         placesDeck.push_back(place3); 
 
@@ -42,9 +42,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place4.setIdPlace("Salle de Sport");
         place4.setIsSafe(false);
         place4.setIsTrapped(true);
-        //place4.setListClues();
-        //place4.setPresentPlayers();
-        //place4.setTrap();
+        place4.setListClues({});
+        place4.setPresentPlayers({});
+        place4.setTrap("");
 
         placesDeck.push_back(place4); 
 
@@ -52,9 +52,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place5.setIdPlace("Spa Botanique");
         place5.setIsSafe(false);
         place5.setIsTrapped(true);
-        //place5.setListClues();
-        //place5.setPresentPlayers();
-        //place5.setTrap();
+        place5.setListClues({});
+        place5.setPresentPlayers({});
+        place5.setTrap("");
 
         placesDeck.push_back(place5); 
 
@@ -62,9 +62,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place6.setIdPlace("Villa Royale");
         place6.setIsSafe(false);
         place6.setIsTrapped(true);
-        //place6.setListClues();
-        //place6.setPresentPlayers();
-        //place6.setTrap();
+        place6.setListClues({});
+        place6.setPresentPlayers({});
+        place6.setTrap("");
 
         placesDeck.push_back(place6); 
 
@@ -72,9 +72,9 @@ std::vector<state::Places> InitPlaces::Init() {
         place7.setIdPlace("Piscine à Débordement");
         place7.setIsSafe(false);
         place7.setIsTrapped(true);
-        //place7.setListClues();
-        //place7.setPresentPlayers();
-        //place7.setTrap();
+        place7.setListClues({});
+        place7.setPresentPlayers({});
+        place7.setTrap("");
 
         placesDeck.push_back(place7); 
 
@@ -82,19 +82,19 @@ std::vector<state::Places> InitPlaces::Init() {
         place8.setIdPlace("Réception");
         place8.setIsSafe(false);
         place8.setIsTrapped(true);
-        //place8.setListClues();
-        //place8.setPresentPlayers();
-        //place8.setTrap();
+        place8.setListClues({});
+        place8.setPresentPlayers({});
+        place8.setTrap("");
 
         placesDeck.push_back(place8); 
 
         state::Places place9;
-        place9.setIdPlace("Terasse d'Observation");
+        place9.setIdPlace("Terrasse d'Observation");
         place9.setIsSafe(false);
         place9.setIsTrapped(true);
-        //place9.setListClues();
-        //place9.setPresentPlayers();
-        //place9.setTrap();
+        place9.setListClues({});
+        place9.setPresentPlayers({});
+        place9.setTrap("");
 
         placesDeck.push_back(place9); 
 
