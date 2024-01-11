a9b45ffab5d3d6fbcf11680bd15341b83b09d623
ilias1313 <iliasdu1313@gmail.com>
2024-01-11 09:22:06 +0100

    Début de code pour l'IA ! ! !

--- src/shared/ai/RandomAI.cpp
+++ src/shared/ai/RandomAI.cpp
@@ -4,3 +4,21 @@
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include "RandomAI.h"
+
+namespace ai {
+
+    RandomAI::RandomAI() {
+       
+    }
+
+    RandomAI::~RandomAI() {
+       
+    }
+
+    void RandomAI::runAI(state::PlayerID ID) {
+      
+    }
+
+}
